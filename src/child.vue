9541eec9c3945c9927c70ba4553e3179333c0323
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 18:49:54 +0530

    Add files via upload

---
+++ src/child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Hello!! I am the first child....</h1>
+    <h2>my name is kartik .</h2>
+    <h2>i am {{age}} years old .</h2>
+    <button @click="changing">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        age : Number,
+        func : Function,
+    },
+    methods:{
+        changing(){
+            this.func();
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+    }
+    div{
+        background-color: burlywood;
+        padding: 2rem;
+    }
+</style>
+
